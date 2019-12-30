0f280e4a65a2541e76764b2a65c1687e1a986ada
PotroChen <1400539511@qq.com>
2019-12-31 00:58:46 +0800

    Upload FSM_Simple

---
+++ FSM-Unity3d/Assets/FSM/Simple/FSMMachine.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CLToolKits.FSM.Simple
+{
+	public class FSMMachine  
+	{
+		bool running = false;
+		List<Status> statuses = new List<Status>();
+		public Status DefaultStatus{get;private set;}
+		public Status CurrentStatus{get;private set;}
+
+		//构造函数中添加默认状态
+		public FSMMachine(Status defaultStatus)
+		{
+			DefaultStatus = defaultStatus;
+			AddStatus(defaultStatus);
+		}
+
+		//添加状态
+		public void AddStatus(Status status)
+		{
+			if(statuses.Contains(status))
+			return;
+
+			statuses.Add(status);
+		}
+
+		//移除状态
+		public void RemoveStatus(Status status)
+		{
+			if(!statuses.Contains(status))
+			return;
+
+			statuses.Remove(status);
+		}
+
+		public void Run()
+		{
+			running = true;
+			CurrentStatus = DefaultStatus;
+			CurrentStatus.OnEnter();
+		}
+
+		//改变状态
+		public void ChangeStatus(Status status)
+		{
+			if(!statuses.Contains(status)||!!running)
+			return;
+			if(CurrentStatus!=null)
+				CurrentStatus.OnLeave();
+
+			status.OnEnter();
+			CurrentStatus = status;
+		}
+	}
+}
---
+++ FSM-Unity3d/Assets/FSM/Simple/Status.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CLToolKits.FSM.Simple
+{
+	public class Status 
+	{
+		public virtual void OnEnter()
+		{
+		}
+
+		public virtual void OnLeave()
+		{
+		}
+	}
+}
